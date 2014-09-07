dc7f7e031c85e298f88f17104f2a03c7ababa385
Sahil Patel <spatel7@berkeley.edu>
2014-09-06 20:56:35 -0700

    Removed support for quakapp/ urls

--- quak/quak/urls.py
+++ quak/quak/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns('',
   url(r'^admin/', include(admin.site.urls)),
   url(r'^$', views.index, name='index'),
   url(r'^create/$', views.create, name='create'),
-  url(r'presentation/(?P<guid>[-\w]+)/$', views.page, name='page'),
-  url(r'incrementVote/$', views.increment_vote, name='increment_vote'),
-  url(r'newQuestion/$', views.new_question, name='new_question'))
+  url(r'^presentation/(?P<guid>[-\w]+)/$', views.page, name='page'),
+  url(r'^incrementVote/$', views.increment_vote, name='increment_vote'),
+  url(r'^newQuestion/$', views.new_question, name='new_question'))
