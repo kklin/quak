61e88cfc02d6545fe251e3e602925df7c702885e
Kevin Lin <kaikai526@gmail.com>
2014-09-06 21:41:13 -0700

    add sorted view

--- quak/quak/urls.py
+++ quak/quak/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
   url(r'^admin/', include(admin.site.urls)),
   url(r'^$', views.index, name='index'),
   url(r'^create/$', views.create, name='create'),
+  url(r'sorted_view/(?P<guid>[-\w]+)/$', views.sorted_view, name='sorted_view'),
   url(r'^presentation/(?P<guid>[-\w]+)/$', views.page, name='page'),
   url(r'^incrementVote/$', views.increment_vote, name='increment_vote'),
   url(r'^newQuestion/$', views.new_question, name='new_question'))
